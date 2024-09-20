8200846185505b4413874dd1ccc58bc6219a25b8
mini2929 <haloyun29@gmail.com>
2024-09-20 17:13:34 +0900

    motion동작 안되는데 왜 안되는지 모르겠음

--- js/script.js
+++ js/script.js
@@ -8,6 +8,16 @@ const frame = document.querySelector('main');
 const panel = frame.querySelector(".panel");
 const btns = frame.querySelectorAll(".btns li");
 
+// 스크립트가 실행되자마자 panel의 넓이값과 높이값을 btns의 갯수에 따라 동적 스타일링
+panel.style.width = 100 * btns.length + "%";
+panel.style.height = "100%";
+
+// 스크립트가 실행되자마자 panel의 자식 li를 모두 찾은 다음 반복을 돌며 btns의 개수에 따라 넓이, 높이 동적 스타일링
+panel.querySelectorAll("li").forEach((li) => {li.style.width = 100 / btns.length +'%';
+    li.style.height = "100%";
+});
+
+
 btns.forEach((btn,idx) => {
     btn.addEventListener("click", () => {
         console.log(idx);
@@ -16,8 +26,7 @@ btns.forEach((btn,idx) => {
     });
 });
 
-// li 태그 안쪽 값 불러오기 getAttribute
+
 // idx 숫자 곱하기 width 값만큼 움직이기
-
-
 // width 퍼센테이지로 바꾸기
+// script에서 width값 지정
